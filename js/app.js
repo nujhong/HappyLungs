52a7d75ac6d6a94e41998105521b14b5a1961c04
nujhong <w.chen-2@hotmail.com>
2016-12-12 00:55:46 +1100

    Make page responsive

--- js/app.js
+++ js/app.js
@@ -1,20 +1,16 @@
 $( document ).ready(function() {
   $(".section").css("min-height", $(window).height() )
+
   $(window).scroll(function () {
-    if ($("#package-middle").isVisible()) {
-      $("#package-middle").addClass("is-active")
-      console.log('Ding ding')
-    }
-    else {
-      $("#package-middle").removeClass("is-active")
-    }
+    $("#package-middle").isVisible() ? ($(".img-animate").addClass("is-active")) : ($(".img-animate").removeClass("is-active"))
   })
 
+  console.log($(".supporting").scrollTop())
 })
 
 $.fn.isVisible = function() {
     var elemTop = this[0].getBoundingClientRect().top;
     var elemBottom = this[0].getBoundingClientRect().bottom;
 
-    return (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return (elemTop >= 0 - this[0].height) && (elemBottom <= window.innerHeight);
 }
